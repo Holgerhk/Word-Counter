65ed4130c9c2097c091cdf392ba9b27fd5f39b74
Holger Krogh <holger.krogh@gmail.com>
2021-09-11 19:43:01 +0200

    again a message

--- script.js
+++ script.js
@@ -12,30 +12,49 @@ function getData() {
 }
 
 function getTime() {
-    let sec = 50;
+    let sec = 30;
     let min = 0;
 
     setInterval(() => {
         if (sec < 59) {
-            sec++;
-            time.innerHTML = `
-            <p class="time">${min}:${sec}</p>
-            `;
+
+            if (sec < 9) {
+                sec++;
+                time.innerHTML = `
+                <p class="time">0${min}:0${sec}</p>
+                `;
+            } else {
+                sec++;
+                time.innerHTML = `
+                <p class="time">0${min}:${sec}</p>
+                `;
+            }
+
         } else if (sec === 59) {
+
             min++;
-            sec = 0;
-            time.innerHTML = `
-            <p class="time">${min}:${sec}</p>
-            `;
+            if (min < 9) {
+                sec = 0;
+                time.innerHTML = `
+                <p class="time">0${min}:0${sec}</p>
+                `;
+            } else {;
+                sec = 0;
+                time.innerHTML = `
+                <p class="time">${min}:${sec}</p>
+                `;
+            }
 
             const data = document.querySelector("textarea").value;
             const realData = data.split(" ");
+            const sentence = data.split(".");
 
             const div = document.createElement("div");
             div.classList.add("info-box");
 
             div.innerHTML = `
                     <p class="min">${min} min</p>
+                    <p class="sentence">${sentence.length - 1} sectences</p>
                     <p class="words">${realData.length} words</p>
             `;
 
